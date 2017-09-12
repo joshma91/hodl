d6fea7c662a07947328291cf03bc4f75d6fa60cf
Joshma91 <josh.ma91@gmail.com>
2017-09-12 11:05:20 -0700

    add -a flag for querying all my coins

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const fetch = require('node-fetch')
 const commandLineArgs = require('command-line-args')
 
 const optionDefinitions=[
-  {name:'coin', alias: 'c', type :String}
+  {name:'coin', alias: 'c', type :String},
+  {name:'all', alias: 'a', type: Boolean}
+]
+
+var myHoldings = [
+  "ethereum",
+  "neo",
+  "zencash",
+  "binance-coin",
+  "metal",
+  "tenx",
+  "rise"
 ]
 
 const parse = commandLineArgs(optionDefinitions)
@@ -11,8 +22,15 @@ const getCurrentPrice = async (id) => {
   const url = `https://api.coinmarketcap.com/v1/ticker/${id}/`
   const obj =  await fetch(url).then(x=> x.json())
   const res = obj[0]
-  console.log(res.price_usd + ' USD | ' + '24h Change: ' + res.percent_change_24h + '%')
+  console.log(res.name + ': ' + res.price_usd + ' USD | ' + '24h Change: ' + res.percent_change_24h + '%')
+  return res
 }
 
-getCurrentPrice(parse.coin)
+const displayHoldings = async (holdings) => {  
+  for(var i=0, j=holdings.length; i<j; i++){
+    getCurrentPrice(holdings[i])
+  }
+}
 
+if (parse.all) displayHoldings(myHoldings)
+if (parse.coin) getCurrentPrice(parse.coin)
