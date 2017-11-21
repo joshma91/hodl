7f9e7f008ad011d60f12325d6cb9785596cfc212
Joshma91 <josh.ma91@gmail.com>
2017-11-20 16:36:10 -0800

    added googlesheets integration - only works for me

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
+#!/usr/bin/env node
+
 const fetch = require('node-fetch')
 const commandLineArgs = require('command-line-args')
+const spreadsheet = require('google-spreadsheet')
+const async = require('async');
 
 const optionDefinitions=[
   {name:'coin', alias: 'c', type :String},
   {name:'all', alias: 'a', type: Boolean}
 ]
 
-var myHoldings = [
-  "ethereum",
-  "neo",
-  "zencash",
-  "binance-coin",
-  "metal",
-  "tenx",
-  "rise"
-]
+const doc = new spreadsheet('1kHiP8DNpy492cEUxp-2bRJrLWHp1mRm_q4rOnILeDag');
+var sheet;
+
+const runAll = () => async.series([
+  function setAuth(step){
+    const creds = require('/home/josh/Downloads/finances-b921f81fc8fc.json');
+    doc.useServiceAccountAuth(creds, step);
+  },
+  function getInfoAndWorksheets(step) {
+    doc.getInfo(function(err, info) {
+      sheet = info.worksheets[0];
+      
+      step();
+    });
+  },function workingWithCells(step) {
+    sheet.getCells({
+      'min-row': 1,
+      'max-row': 10,
+      'min-col': 1,
+      'max-col':7,
+      'return-empty': true
+    }, function(err, cells) {
+      
+      cells.map(x => {
+        if(x.col == 1 && x.value.length > 0){
+          getCurrentPrice(x.value)
+        }
+      });
+      step();
+    });
+  }
+])
 
 const parse = commandLineArgs(optionDefinitions)
 
@@ -22,8 +49,8 @@ const getCurrentPrice = async (id) => {
   const url = `https://api.coinmarketcap.com/v1/ticker/${id}/`
   const obj =  await fetch(url).then(x=> x.json())
   const res = obj[0]
-  console.log(res.name + ': ' + res.price_usd + ' USD | ' + '24h Change: ' + res.percent_change_24h + '%')
-  return res
+  console.log(res.name + ': ' + res.price_usd + ' USD | ' + '24h Change: ' + res.percent_change_24h + '% | '  + '7D Change: ' + res.percent_change_7d + '% |' )
+
 }
 
 const displayHoldings = async (holdings) => {  
@@ -32,5 +59,8 @@ const displayHoldings = async (holdings) => {
   }
 }
 
-if (parse.all) displayHoldings(myHoldings)
-if (parse.coin) getCurrentPrice(parse.coin)
+if (parse.coin){
+    getCurrentPrice(parse.coin)
+} else {
+    runAll();
+}
