185efc24040d5e2e00e972f1bd65edbd86071dcc
Joshma91 <josh.ma91@gmail.com>
2017-11-21 23:09:33 -0800

    yah

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const async = require('async');
 
 const optionDefinitions=[
   {name:'coin', alias: 'c', type :String},
-  {name:'all', alias: 'a', type: Boolean}
+  {name:'all', alias: 'a', type: Boolean},
+  {name:'worth', alias: 'w', type: Boolean}
 ]
 
 const doc = new spreadsheet('1kHiP8DNpy492cEUxp-2bRJrLWHp1mRm_q4rOnILeDag');
+const parse = commandLineArgs(optionDefinitions)
 var sheet;
+var total = 0;
 
 const runAll = () => async.series([
   function setAuth(step){
@@ -33,30 +36,33 @@ const runAll = () => async.series([
       'return-empty': true
     }, function(err, cells) {
       
-      cells.map(x => {
+      let promises = cells.map( async (x, index, array) => {
         if(x.col == 1 && x.value.length > 0){
-          getCurrentPrice(x.value)
+          const numCoins = array[index+1].value;
+          await getCurrentPrice(x.value, numCoins);
         }
-      });
+      })
+      Promise.all(promises).then(() => console.log('Total profit: $' + parseFloat(total-35820).toFixed(2)));
+            
       step();
     });
   }
 ])
 
-const parse = commandLineArgs(optionDefinitions)
-
-const getCurrentPrice = async (id) => {
+const getCurrentPrice = async (id, numCoins) => {
   const url = `https://api.coinmarketcap.com/v1/ticker/${id}/`
   const obj =  await fetch(url).then(x=> x.json())
   const res = obj[0]
-  console.log(res.name + ': ' + res.price_usd + ' USD | ' + '24h Change: ' + res.percent_change_24h + '% | '  + '7D Change: ' + res.percent_change_7d + '% |' )
-
-}
-
-const displayHoldings = async (holdings) => {  
-  for(var i=0, j=holdings.length; i<j; i++){
-    getCurrentPrice(holdings[i])
+  
+  if(parse.worth) {
+    const numCoinsInFloat = parseFloat(numCoins.replace(',', ''))
+    const valueInCAD = numCoinsInFloat*res.price_usd*1.27;
+    outStr = res.name + ': ' + res.price_usd + ' USD | ' + '24h Change: ' + res.percent_change_24h + '% | '  + 'Value: ' + valueInCAD;
+    total += valueInCAD;
+  } else {
+    outStr = res.name + ': ' + res.price_usd + ' USD | ' + '24h Change: ' + res.percent_change_24h + '%'; 
   }
+  console.log(outStr)
 }
 
 if (parse.coin){
